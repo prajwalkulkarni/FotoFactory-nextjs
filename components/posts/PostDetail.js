4fda32d1dac39104bdc2f8387d4d5517c1ec6d43
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-10-05 18:27:29 +0530

    Club multiple states to single hook object state

--- components/posts/PostDetail.js
+++ components/posts/PostDetail.js
@@ -1,69 +1,92 @@
 import classes from './PostDetail.module.css'
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Input from '@material-ui/core/input'
-// import SendIcon from '@material-ui/icons-material/Send';
+import Head from 'next/head'
 import SendIcon from '@mui/icons-material/Send';
 import { IconButton } from '@material-ui/core';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 export default function PostDetail(props){
 
-    const [comments,commentList]= useState([])
-
+    // const [comments,commentList]= useState([])
+    // const [addingComment,setAddingComment] = useState(false)
+    
+    const [comments,setCommentProps] = useState({
+        comments:[],
+        addingComment:false,
+        currVal:''
+    })
     function addComment(e){
         e.preventDefault()
         // console.log(e.target.comment.value)
-        commentList([...comments,e.target.comment.value])
-        props.onAddComment(e.target.comment.value)
+        
+        setCommentProps({...comments,addingComment:true})
+        props.onAddComment(e.target.comment.value).then(res=>{
+            setCommentProps({...comments,currVal:''})
+            setCommentProps({...comments,addingComment:false})
+            setCommentProps({...comments,comments:[...comments.comments,e.target.comment.value]})    
+        }).catch(err=>{
+            setCommentProps({...comments,addingComment:false})
+            alert(err)
+        })
 
 
         
     }
 
     useEffect(()=>{
-        commentList(props.comments)
+        setCommentProps({...comments,comments:props.comments})
     },[])
     
     
     return(
-        <div className={classes.parentGrid}>
-            <div className={classes.imgContainer}>
-                    <img src={props.image} className={classes.image}/>
+
+        <React.Fragment>
+            <Head>
+                <title>Foto factory - {props.title}</title>
                 
-            </div>
+            </Head>
 
-            <div className={classes.contentContainer}>
-                <h2 style={{paddingLeft:10}}>{props.title}</h2>
-                <hr style={{ background: 'black', color: 'black', margin: 0 }} />
-                <div className={classes.scrollable}>
-                    <p style={{paddingLeft:10}}>{props.caption}</p>
-                    <hr style={{ background: 'black', color: 'black', margin: 0 }} />
-                    <div className={classes.comments}>
-                        {comments.length>0?comments.map(comment=><p style={{paddingLeft:10}}  key={props.id}>{comment}</p>):<h1>No comments yet</h1>}
-
-                    </div>
+            <div className={classes.parentGrid}>
+                <div className={classes.imgContainer}>
+                    <img src={props.image} className={classes.image} />
 
                 </div>
-                <hr style={{ background: 'black', color: 'black', margin: 0 }} />
-                <form className={classes.commentAdder} onSubmit={addComment} autoComplete="off">
-                   
 
-                    
-                    <Input
-                        id="comment"
-                        style={{width:'90%',marginLeft:'10px',marginRight:'10px'}}
-                        placeholder='Type something...'
-                        endAdornment={
-                            <IconButton aria-label="send" type='submit'>
-                                <SendIcon/>
-                            </IconButton>
-                        }
-                    />
-                </form>
+                <div className={classes.contentContainer}>
+                    <h2 style={{ paddingLeft: 10 }}>{props.title}</h2>
+                    <hr style={{ background: 'black', color: 'black', margin: 0 }} />
+                    <div className={classes.scrollable}>
+                        <p style={{ paddingLeft: 10 }}>{props.caption}</p>
+                        <hr style={{ background: 'black', color: 'black', margin: 0 }} />
+                        <div className={classes.comments}>
+                            {comments.comments.length > 0 ? comments.comments.map(comment => <p style={{ paddingLeft: 10 }} key={props.id}>{comment}</p>) : <h1>No comments yet</h1>}
+                            {comments.addingComment&&<Box sx={{ display: 'flex',justifyContent:'center',padding:'5px' }}><CircularProgress /></Box>}
+                        </div>
+
+                    </div>
+                    <hr style={{ background: 'black', color: 'black', margin: 0 }} />
+                    <form className={classes.commentAdder} onSubmit={addComment} autoComplete="off">
+                        <Input
+                            id="comment"
+                            style={{ width: '90%', marginLeft: '10px', marginRight: '10px' }}
+                            placeholder='Type something...'
+                            value={comments.currVal}
+                            required
+                            onChange={(e)=>{setCommentProps({...comments,currVal:e.target.value})}}
+                            endAdornment={
+                                <IconButton aria-label="send" type='submit'>
+                                    <SendIcon />
+                                </IconButton>
+                            }
+                        />
+                    </form>
+
+                </div>
 
             </div>
+        </React.Fragment>
 
-        </div>
-
-        
     )
 }
