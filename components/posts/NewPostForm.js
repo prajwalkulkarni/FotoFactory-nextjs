43f5cc287e097a204b1d210ba5e861523b9b7d78
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-10-05 10:46:56 +0530

    Responsive design

--- components/posts/NewPostForm.js
+++ components/posts/NewPostForm.js
@@ -4,20 +4,27 @@ import classes from './NewPostForm.module.css';
 import {getDownloadURL,ref,uploadBytes} from 'firebase/storage'
 import { storage } from '../../sdk/firebase-config';
 // import { storage } from '../../sdk/firebase-config';
-
+import useValidation from '../../hooks/form-validation';
 function NewPostForm(props) {
-  const titleInputRef = useRef();
-  const imageInputRef = useRef();
-  const descriptionInputRef = useRef();
   
-  // const [currImg,setCurrImg] = useState('https://unctad.org/sites/default/files/2021-08/2021-08-03_commentary_tourism_1200x675.jpg')
+  const imageInputRef = useRef();
+  
+  const {input:inputTitleValue,inputIsValid:titleIsValid,inputIsInValid:titleIsInvalid,callbackHandler:setTitle,toggleActiveHandler:toggleTitleActiveHandler} = useValidation((arg)=>arg.trim().length>1)
+  const {input:inputCaptionValue,inputIsValid:captionIsValid,inputIsInValid:captionIsInvalid,callbackHandler:setCaption,toggleActiveHandler:toggleCaptionActiveHandler} = useValidation((arg)=>arg.trim().length>15)
+  
+  
+  let formIsValid = false
+  const imgIsValid = false
+
+  if(titleIsValid&&captionIsValid){
+    formIsValid=true
+  }
   function submitHandler(event) {
     event.preventDefault();
 
-    const enteredTitle = titleInputRef.current.value;
-    const enteredImage = imageInputRef.current.value;
-    
-    const enteredDescription = descriptionInputRef.current.value;
+    if(!formIsValid){
+      return;
+    }
     const storageRef = ref(storage,'fotofactory/'+image.files[0].name)
     uploadBytes(storageRef,image.files[0]).then((snapshot)=>{
       // console.log(snapshot)
@@ -25,9 +32,9 @@ function NewPostForm(props) {
       getDownloadURL(ref(storageRef)).then(url=>{
         // console.log("WTFFF")
         const meetupData = {
-          title: enteredTitle,
+          title: inputTitleValue,
           image: url,
-          description: enteredDescription,
+          description: inputCaptionValue,
           comments:[]
         };
         props.onAddPost(meetupData);
@@ -39,29 +46,46 @@ function NewPostForm(props) {
     
   }
 
+  let fieldClassName = classes.control
+  if(titleIsInvalid){
+    fieldClassName = `${classes.control} ${classes.error}`
+  }
+
+  let captionFieldClassName = classes.control
+  if(captionIsInvalid){
+    captionFieldClassName = `${classes.control} ${classes.error}`
+  }
+
   return (
     <Card>
       <form className={classes.form} onSubmit={submitHandler}>
-        <div className={classes.control}>
+        <div className={fieldClassName}>
           <label htmlFor='title'>Foto Title*</label>
-          <input type='text' required id='title' ref={titleInputRef} />
+          <input type='text' required id='title' 
+          onBlur={toggleTitleActiveHandler} 
+          onChange={(e)=>{setTitle(e.target.value)}} 
+          value={inputTitleValue}/>
+          {titleIsInvalid&&<p className={classes.errorMessage}>Invalid title.</p>}
         </div>
         <div className={classes.control}>
           <label htmlFor='image'>Image*</label>
           <input type='file' accept="image/*" required id='image' ref={imageInputRef} />
         </div>
         
-        <div className={classes.control}>
+        <div className={captionFieldClassName}>
           <label htmlFor='caption'>Caption*</label>
           <textarea
             id='description'
+            value={inputCaptionValue}
+            onChange={(e)=>{setCaption(e.target.value)}}
+            onBlur={toggleCaptionActiveHandler}
             required
             rows='5'
-            ref={descriptionInputRef}
           ></textarea>
+          {captionIsInvalid&&<p className={classes.errorMessage}>Caption should have a minimum length of 15 characters.</p>}
         </div>
         <div className={classes.actions}>
-          <button>Add Post</button>
+          <button disabled={!formIsValid}>Add Post</button>
         </div>
       </form>
 
